b83e88fb55198e8ad1256065424f2cd70e10ad26
Guilherme Miranda <guilherme_de_oliveira_n1@hotmail.com>
2024-04-11 10:02:56 -0300

    chore: create project and add redux

---
+++ slices/navSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInfo: null,
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducers: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInfo: (state, action) => {
+      state.travelTimeInfo = action.payload;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInfo } =
+  navSlice.actions;
+
+export const selectOrigin = (state) => state.origin;
+export const selectDestination = (state) => state.destination;
+export const selectTravelTimeInfo = (state) => state.travelTimeInfo;
+
+export default navSlice.reducer;
